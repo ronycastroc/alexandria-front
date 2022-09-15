e105e79a0d66bbf5fdc9fb4f1ddff1bae3543760
ThiagoCCR <thiago.ccr96@gmail.com>
2022-09-14 21:12:51 -0300

    feat bookbox layout

--- src/components/Pages/Home.js
+++ src/components/Pages/Home.js
@@ -3,6 +3,7 @@ import Header from "../Header";
 import { IoChevronForwardSharp, IoChevronBackSharp } from "react-icons/io5";
 import { IconContext } from "react-icons";
 import { useRef, useCallback } from "react";
+import BookBox from "../BookBox";
 
 export default function Home() {
   const carousel = useRef(null);
@@ -37,12 +38,18 @@ export default function Home() {
             </IconContext.Provider>
           </CarouselButton>
           <BooksContainer ref={carousel}>
-            <div></div>
-            <div></div>
-            <div></div>
-            <div></div>
-            <div></div>
-            <div></div>
+            <div>
+              <BookBox />
+            </div>
+            <div>
+              <BookBox />
+            </div>
+            <div>
+              <BookBox />
+            </div>
+            <div>
+              <BookBox />
+            </div>
           </BooksContainer>
           <CarouselButton onClick={(e) => handleRightClick(e)}>
             <IconContext.Provider
@@ -75,7 +82,6 @@ const Wrapper = styled.div`
 
 const Container = styled.div`
   width: 100%;
-  height: 400px;
   margin-top: 150px;
   display: flex;
   flex-direction: column;
@@ -84,7 +90,7 @@ const Container = styled.div`
   h1 {
     font-size: 30px;
     font-weight: 700;
-    margin-bottom: 20px;
+    margin-bottom: 30px;
   }
 `;
 
@@ -94,24 +100,29 @@ const Carousel = styled.div`
   align-items: center;
   justify-content: center;
   width: 100%;
-  height: 250px;
 `;
 
 const BooksContainer = styled.div`
-  background-color: blue;
   width: 1000px;
-  height: 200px;
+  height: 500px;
   overflow-x: auto;
   padding: 20px;
   overflow-y: hidden;
   white-space: nowrap;
   scroll-behavior: smooth;
-  div {
+  > div {
     display: inline-block;
-    margin-right: 15px;
-    width: 200px;
-    height: 150px;
-    background-color: orange;
+    margin-right: 20px;
+    width: 250px;
+    height: 400px;
+    cursor: pointer;
+    h1 {
+      margin-bottom: 0px;
+    }
+    &:hover {
+      top: 0;
+      box-shadow: 0px 2px 4px 2px rgba(0, 0, 0, 0.1);
+    }
   }
   ::-webkit-scrollbar {
     display: none;
@@ -120,9 +131,12 @@ const BooksContainer = styled.div`
 
 const CarouselButton = styled.div`
   width: 100px;
-  height: 200px;
+  height: 50px;
   display: flex;
   align-items: center;
   justify-content: center;
   cursor: pointer;
+  &:hover {
+    opacity: 0.7;
+  }
 `;
