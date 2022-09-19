afdbdb45dc75cc5e5dd5d52c3375fdc93b73293b
ThiagoCCR <thiago.ccr96@gmail.com>
2022-09-19 17:38:03 -0300

    fix busca categorias

--- src/components/MobileHeader.js
+++ src/components/MobileHeader.js
@@ -138,7 +138,7 @@ export default function MobileHeader() {
               Fantasia
             </span>
             <span onClick={() => navigate("/categorias/Terror")}>Terror</span>
-            <span onClick={() => navigate("/categorias/Auto-ajuda")}>
+            <span onClick={() => navigate("/categorias/Auto-Ajuda")}>
               Auto Ajuda
             </span>
             <span onClick={() => navigate("/categorias/Ficção-Cientifica")}>
