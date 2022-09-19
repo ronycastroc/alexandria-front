7c957f78aaae637af94e276778fb35b50af708f4
ThiagoCCR <thiago.ccr96@gmail.com>
2022-09-19 16:02:34 -0300

    a

--- src/components/Header.js
+++ src/components/Header.js
@@ -98,7 +98,7 @@ export default function Header() {
 const Wrapper = styled.div`
   width: 100%;
   background-color: #0a100d;
-  height: 80px;
+  height: 80px !important;
   display: flex;
   flex-direction: row;
   align-items: center;
